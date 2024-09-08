bca0bd1676c00821f673d7988c4fbc0c30c16543
charlieweinberger <charlieweinberger05@gmail.com>
2024-09-08 00:52:29 -0700

    got rid of prisma, although I haven't started migrating to supabase.js

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,22 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from "./../../../../prisma/client";
 
 export async function GET() {
-  console.log("inside get");
-  try {
-    console.log("here");
-    const data = await prisma.post.findMany();
-    console.log("data");
-    return NextResponse.json(data);
-  } catch (error) {
-    console.log(error);
-  }
+
 }
 
 export async function POST(request: NextRequest, response: NextResponse) {
-  const post = await request.json();
-  const data = await prisma.post.create({
-    data: { title: post.title }
-  });
-  return NextResponse.json(data); 
+
 }
